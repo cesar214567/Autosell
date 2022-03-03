363545870bbaa99086fd5468294a4ff92815f65b
cesar214567 <camgcamg11@gmail.com>
2022-03-03 17:22:16 -0500

    hiding password

--- src/main/java/com/example/autosell/entities/Users.java
+++ src/main/java/com/example/autosell/entities/Users.java
@@ -1,5 +1,7 @@
 package com.example.autosell.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -36,10 +38,12 @@ public class Users {
         this.email = email;
     }
 
+    @JsonIgnore
     public String getPassword() {
         return password;
     }
 
+    @JsonProperty
     public void setPassword(String password) {
         this.password = password;
     }
